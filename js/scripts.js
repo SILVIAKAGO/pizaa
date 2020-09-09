5b9e1782fe635118bd1c2eaaf95aaeaf4f233107
silviakago <deejaykayslyk@gmail.com>
2020-09-09 14:50:36 +0300

    added scripts.js method to reset

--- js/scripts.js
+++ js/scripts.js
@@ -50,4 +50,11 @@ $(document).ready(function () {
         prompt("enter your location");
         alert("Your pizza will be delivered");
 
+        //a method to reset the form after all operations have been completed
+        $("#text-center").reset();
+
+        event.preventDefault();
+    });
+});
+
 
